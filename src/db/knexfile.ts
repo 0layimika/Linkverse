edc04b556df09d4766c3b0b15326257bd1b2888a
0layimika <olakay739@gmail.com>
2026-01-25 18:02:08 +0100

    build scripts

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 export default {
     development: {
         client: 'pg',
-        connection: process.env.DATABASE_URL || 'postgresql://olayimika:password@localhost:5432/creatorlink',
+        connection: process.env.DATABASE_URL || 'postgresql://postgres:GzaBjIfXHlrbzwBHuoXfcKZUNjVATODo@switchyard.proxy.rlwy.net:42982/railway',
         migrations: {
             directory: '../migrations',
         },
