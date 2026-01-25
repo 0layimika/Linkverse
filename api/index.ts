d9ef5e0382edfdafaa0832c361a9946b63bace22
0layimika <olakay739@gmail.com>
2026-01-25 19:08:45 +0100

    hope this works

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,13 @@ import app from '../src/app';
 import serverless from 'serverless-http';
 import { connectDB } from '../src/db/connectDB';
 
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    return res.sendStatus(200);
+});
 // Connect to DB at startup
 connectDB().then(() => {
     console.log("✅ Database connected");
